fbe02fd53a4d131eb20278e829dc58fc1a9c7a17
Gixu572 <gautam20219102@gmail.com>
2022-06-02 17:57:25 +0530

    Meow

---
+++ src/main/java/me/gixu/meow/Meow.java
@@ -0,0 +1,17 @@
+package me.gixu.meow;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Meow extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Thanks For Using This Plugin Made By Gautam And Anshu..!!")
+
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("Made By Anshu And Gautam..!! Thanks For Using :)")
+    }
+}
